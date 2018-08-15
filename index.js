63a1e846a4bc8cd0248e33b01db8bb956f608d66
Marcelo Franco <marcelo.franco.br@gmail.com>
2018-08-15 11:52:44 -0300

    comunicação entre processos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const electron = require('electron');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 
 app.on('ready', ( ) => {
     const janelaPrincipal = new BrowserWindow({});
     janelaPrincipal.loadURL(`file://${__dirname}/index.html`);
 });
 
+ipcMain.on('obterDimensoesDaImagem', () => {
+
+});
